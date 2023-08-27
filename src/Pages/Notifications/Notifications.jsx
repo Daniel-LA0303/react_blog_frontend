c9de90a4a735cf8ecbd42ffc1911f0d4f6ce05dc
Daniel-LA0303 <03daniel030399@gmail.com>
2023-08-26 19:51:24 -0600

    changes in Notifications page

--- src/Pages/Notifications/Notifications.jsx
+++ src/Pages/Notifications/Notifications.jsx
@@ -1,21 +1,44 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Sidebar from '../../components/Sidebar/Sidebar'
 import { TabContext, TabList, TabPanel } from "@mui/lab";
 import { Box, Tab } from "@mui/material";
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 const Notifications = () => {
 
+    const params = useParams();
+
+    const link = useSelector(state => state.posts.linkBaseBackend);
+
     const [activeTab, setActiveTab] = useState("1");
     const [tabM, setTabM] = useState(false);
+
+    useEffect(() => {
+      
+      const getUserNotifications = async() => {
+        try {
+          const res = await axios.get(`${link}/users/user-notifications/${params.id}`);
+          console.log(res.data);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+
+      getUserNotifications();
+    }, []);
 
     const handleTabChange = (event, newValue) => {
         setActiveTab(newValue);
       };
 
+
+
   return (
     <div>
       <Sidebar />
-      <div class="">
+      <div class="w-full md:w-11/12 lg:w-11/12 xl:w-8/12 mx-auto mb-10">
         <aside className="w-full text-white  bg-[#313131] border-l border-[#525252] ">
           <TabContext value={activeTab}>
             {/* <div className="icon-container"> */}
