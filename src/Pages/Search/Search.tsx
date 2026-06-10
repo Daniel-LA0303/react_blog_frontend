abd669d0b6429f71acf73c5a8e4f6dd41fb26771
Daniel-LA0303 <03daniel030399@gmail.com>
2026-06-09 19:32:44 -0600

    feat: changes inserach

--- src/Pages/Search/Search.tsx
+++ src/Pages/Search/Search.tsx
@@ -14,6 +14,7 @@ import { useNavigate, useParams } from 'react-router-dom'
 
 import axios from 'axios'
 import useGlobalDataContext from '../../context/hooks/useGlobalDataContext'
+import clientAuthAxios from '../../services/clientAuthAxios'
 
 const Search = () => {
 
@@ -39,7 +40,7 @@ const Search = () => {
 
   useEffect(() => {
     setLoading(true);
-    axios.get(`${globalData.link}/pages/global/${params.id}`)
+    clientAuthAxios.get(`${globalData.link}/pages/global/${params.id}`)
       .then((response) => {
         // first info
         setCatFilter(response.data.categories.data);
